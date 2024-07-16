d6bb8f057d7f18a4e6f33dd6378612f8d07f428f
Subhobrata Dey <sbcd90@gmail.com>
2024-07-16 11:57:40 -0700

    fix minor bug in lock handler (#1486)
    * fix minor bug in lock handler
    
    Signed-off-by: Subhobrata Dey <sbcd90@gmail.com>
    
    * dummy commit
    
    Signed-off-by: Subhobrata Dey <sbcd90@gmail.com>
    
    ---------
    
    Signed-off-by: Subhobrata Dey <sbcd90@gmail.com>

--- alerting/src/main/kotlin/org/opensearch/alerting/MonitorRunnerService.kt
+++ alerting/src/main/kotlin/org/opensearch/alerting/MonitorRunnerService.kt
@@ -346,7 +346,7 @@ object MonitorRunnerService : JobRunner, CoroutineScope, AbstractLifecycleCompon
                         }
                     } finally {
                         monitorCtx.client!!.suspendUntil<Client, Boolean> { monitorCtx.lockService!!.release(lock, it) }
-                        logger.debug("lock ${lock!!.lockId} released")
+                        logger.debug("lock ${lock?.lockId} released")
                     }
                 }
             }
@@ -378,7 +378,7 @@ object MonitorRunnerService : JobRunner, CoroutineScope, AbstractLifecycleCompon
                         }
                     } finally {
                         monitorCtx.client!!.suspendUntil<Client, Boolean> { monitorCtx.lockService!!.release(lock, it) }
-                        logger.debug("lock ${lock!!.lockId} released")
+                        logger.debug("lock ${lock?.lockId} released")
                     }
                 }
             }
